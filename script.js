8b59d115d8ebc5a24c41abd7b9ccbb6437e8d948
LucilaAllende <abigail_lucii@hotmail.com>
2024-02-15 11:28:46 -0300

    Endlessly scrolling backgrounds technique 1

--- script.js
+++ script.js
@@ -3,3 +3,23 @@ const ctx = canvas.getContext('2d');
 const CANVAS_WIDTH = canvas.width = 800;
 const CANVAS_HEIGHT = canvas.height = 700;
 
+let gameSpeed = 5;
+
+const backgroundLayer1 = new Image();
+backgroundLayer1.src = 'assets/layer-4.png';
+
+let x = 0;
+let x2 = 2400;
+
+function animate(){
+  ctx.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
+  ctx.drawImage(backgroundLayer1, x, 0);
+  ctx.drawImage(backgroundLayer1, x2, 0);
+  if (x < -2400) x = 2400 + x2 - gameSpeed;
+  else x-= gameSpeed;
+  if (x2 < -2400) x2 = 2400 + x - gameSpeed;
+  else x2-= gameSpeed;
+  requestAnimationFrame(animate);
+}
+
+animate();
